c3850f40b8ffccb403470e6bfb4ec797b716a9ab
Che-Wei Lin <linton.tw@gmail.com>
2016-05-12 20:16:05 +0800

    controller refactor

--- controllers/account.rb
+++ controllers/account.rb
@@ -1,8 +1,9 @@
 require 'sinatra'
 
-# Basec class for ShareFiles Web Application
+# Base class for ShareFilesApp Web Application
 class ShareFilesApp < Sinatra::Base
-  use Rack::Session::Cookie, expire_after: 2_592_000 # One month in seconds
+  use Rack::Session::Cookie, coder: CookieEncoder.new,
+                             let_coder_handle_secure_encoding: true
 
   set :views, File.expand_path('../../views', __FILE__)
 
@@ -12,5 +13,44 @@ class ShareFilesApp < Sinatra::Base
 
   get '/' do
     slim :home
+  end
+end
+
+
+require 'sinatra'
+
+# Base class for ShareFilesApp Web Application
+class ShareFilesApp < Sinatra::Base
+  get '/account/:username' do
+    if @current_account && @current_account['username'] == params[:username]
+      slim(:account)
+    else
+      slim(:login)
+    end
+  end
+
+  get '/login/?' do
+    slim :login
+  end
+
+  post '/login/?' do
+    username = params[:username]
+    password = params[:password]
+
+    @current_account = FindAuthenticatedAccount.call(
+      username: username, password: password)
+
+    if @current_account
+      session[:current_account] = @current_account
+      slim :home
+    else
+      slim :login
+    end
+  end
+
+  get '/logout/?' do
+    @current_account = nil
+    session[:current_account] = nil
+    slim :login
   end
 end

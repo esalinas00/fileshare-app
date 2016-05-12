252cc58b7d834ed189d5add931c66d92514a5cab
Che-Wei Lin <linton.tw@gmail.com>
2016-05-12 19:51:51 +0800

    remove unused variable

--- services/find_authenticated_account.rb
+++ services/find_authenticated_account.rb
@@ -2,8 +2,6 @@ require 'http'
 
 # Returns an authenticated user, or nil
 class FindAuthenticatedAccount
-  HOST = 'http://fileshare-api.herokuapp.com/api/v1'
-
   def self.call(username:, password:)
     response = HTTP.get("#{ENV['API_HOST']}/accounts/#{username}/authenticate",
                         params: {password: password})

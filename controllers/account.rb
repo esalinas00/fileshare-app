5b7c4d39b8f17a96b834fd98f4f4053103676edd
Che-Wei Lin <linton.tw@gmail.com>
2016-05-12 20:36:31 +0800

    fixed account controller

--- controllers/account.rb
+++ controllers/account.rb
@@ -1,25 +1,6 @@
 require 'sinatra'
 
-# Base class for ShareFilesApp Web Application
-class ShareFilesApp < Sinatra::Base
-  use Rack::Session::Cookie, coder: CookieEncoder.new,
-                             let_coder_handle_secure_encoding: true
-
-  set :views, File.expand_path('../../views', __FILE__)
-
-  before do
-    @current_account = session[:current_account]
-  end
-
-  get '/' do
-    slim :home
-  end
-end
-
-
-require 'sinatra'
-
-# Base class for ShareFilesApp Web Application
+# Base class for ShareFile Web Application
 class ShareFilesApp < Sinatra::Base
   get '/account/:username' do
     if @current_account && @current_account['username'] == params[:username]

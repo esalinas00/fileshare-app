7c22aeafc90ed77181ea7421c030ecaac2e34e03
Che-Wei Lin <linton.tw@gmail.com>
2016-05-12 18:54:58 +0800

    service objects

--- services/find_authenticated_account.rb
+++ services/find_authenticated_account.rb
@@ -5,7 +5,7 @@ class FindAuthenticatedAccount
   HOST = 'http://fileshare-api.herokuapp.com/api/v1'
 
   def self.call(username:, password:)
-    response = HTTP.get("#{HOST}/accounts/#{username}/authenticate",
+    response = HTTP.get("#{ENV['API_HOST']}/accounts/#{username}/authenticate",
                         params: {password: password})
     response.code == 200 ? JSON.parse(response) : nil
   end

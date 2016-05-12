5206f72322ca63e2d9cd4a3ebb317b7f7a4eb79f
Robert-Rino <a901002666@gmail.com>
2016-05-12 19:18:10 +0800

    add controllers

--- controllers/account.rb
+++ controllers/account.rb
@@ -2,15 +2,36 @@ require 'sinatra'
 
 # Basec class for ShareFiles Web Application
 class ShareFilesApp < Sinatra::Base
-  use Rack::Session::Cookie, expire_after: 2_592_000 # One month in seconds
-
-  set :views, File.expand_path('../../views', __FILE__)
-
-  before do
-    @current_account = session[:current_account]
+  get '/account/:username' do
+    if @current_account && @current_account['username'] == params[:username]
+      slim(:account)
+    else
+      slim(:login)
+    end
   end
 
-  get '/' do
-    slim :home
-  end
+  get '/login/?' do
+   slim :login
+ end
+
+ post '/login/?' do
+   username = params[:username]
+   password = params[:password]
+
+   @current_account = FindAuthenticatedAccount.call(
+     username: username, password: password)
+
+   if @current_account
+     session[:current_account] = @current_account
+     slim :home
+   else
+     slim :login
+   end
+ end
+
+ get '/logout/?' do
+   @current_account = nil
+   session[:current_account] = nil
+   slim :login
+ end
 end
--- controllers/base.rb
+++ controllers/base.rb
@@ -2,40 +2,17 @@ require 'sinatra'
 
 # Base class for ShareFiles Web Application
 class ShareFilesApp < Sinatra::Base
-  get '/login/?' do
-    slim :login
+  use Rack::Session::Cookie, coder: CookieEncoder.new,
+                             let_coder_handle_secure_encoding: true
+
+  set :views, File.expand_path('../../views', __FILE__)
+
+  before do
+    @current_account = session[:current_account]
   end
 
-  post '/login/?' do
-    username = params[:username]
-    password = params[:password]
-
-    @current_account = FindAuthenticatedAccount.call(
-      username: username, password: password)
-
-    if @current_account
-      session[:current_account] = @current_account
-      slim :home
-    else
-      slim :login
-    end
+  get '/' do
+    slim :home
   end
 
-  get '/logout/?' do
-    @current_account = nil
-    session[:current_account] = nil
-    slim :login
-  end
-
-  get '/account/:username' do
-    if @current_account && @current_account['username'] == params[:username]
-      slim(:account)
-    else
-      slim(:login)
-    end
-  end
-
-  get '/register' do
-    slim(:register)
-  end
 end

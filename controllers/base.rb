4624a47f6efead0355f143fad54114c70c25a96c
Che-Wei Lin <linton.tw@gmail.com>
2016-05-19 16:46:22 +0800

    move ssl-enforcer in base controller

--- controllers/base.rb
+++ controllers/base.rb
@@ -1,14 +1,25 @@
 require 'sinatra'
+require 'rack-flash'
+require 'rack/ssl-enforcer'
 
 # Base class for ShareFilesApp Web Application
 class ShareFilesApp < Sinatra::Base
   enable :logging
+
   use Rack::Session::Cookie, secret: ENV['MSG_KEY']
+  use Rack::Flash
+
+  configure :production do
+    use Rack::SslEnforcer
+  end
 
   set :views, File.expand_path('../../views', __FILE__)
+  set :public_dir, File.expand_path('../../public', __FILE__)
 
   before do
-    @current_account = session[:current_account]
+    if session[:current_account]
+      @current_account = SecureMessage.decrypt(session[:current_account])
+    end
   end
 
   get '/' do

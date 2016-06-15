d58e84d7294efa14fd73dfde8470ab8bcb5237a9
Che-Wei Lin <linton.tw@gmail.com>
2016-06-15 16:05:54 +0800

    CreateNewFile using base64 encode string

--- services/create_new_file.rb
+++ services/create_new_file.rb
@@ -1,14 +1,16 @@
 require 'http'
+require 'base64'
 
 class CreateNewFile
   def self.call(folder_id:, filename:, description: nil, document:)
     file_url = "#{ENV['API_HOST']}/folders/#{folder_id}/files"
+    base64_encode_document = Base64.strict_encode64(document)
     response = HTTP.accept('application/json')
                    .post(file_url,
                          json: {
                            filename: filename,
                            description: description,
-                           document: document
+                           document: base64_encode_document
                            })
     new_file = response.parse
     response.code == 201 ? new_file : nil

a6a13240e10d51ae27c644080d528aba23c0aef5
Che-Wei Lin <linton.tw@gmail.com>
2016-06-16 02:12:06 +0800

    extract folder details

--- services/get_folder_details.rb
+++ services/get_folder_details.rb
@@ -11,20 +11,37 @@ class GetFolderDetails
   private_class_method
 
   def self.extract_folder_details(folder_data)
-    folder = folder_data['data']
-    content = folder_data['relationships']
+    content = folder_data['relationships']['files']
 
     files = content.map do |f|
-      {
-        id: f['id'],
-        filename: f['data']['filename'],
-        description: f['data']['description']
-      }
+      { 'id' => f['id'],
+        'filename' => f['data']['filename'],
+        'description' => f['data']['description'] }
     end
 
-    { id: folder['id'],
-      name: folder['attributes']['name'],
-      folder_url: folder['attributes']['folder_url'],
-      files: files }
+    { 'id' => folder_data['id'],
+      'name' => folder_data['attributes']['name'],
+      'folder_url' => folder_data['attributes']['folder_url'],
+      'files' => files }
+      .merge(folder_data['attributes'])
+      .merge(folder_data['relationships'])
   end
+
+  # def self.extract_folder_details(folder_data)
+  #   folder = folder_data['data']
+  #   content = folder_data['relationships']
+  #   # {"type"=>"folder", "id"=>2, "attributes"=>{"name"=>"Gembucket", "folder_url"=>nil}, "relationships"=>{"owner"=>nil, "collaborators"=>[], "files"=>[]}}
+  #   files = content.map do |f|
+  #     {
+  #       id: f['id'],
+  #       filename: f['data']['filename'],
+  #       description: f['data']['description']
+  #     }
+  #   end
+  #
+  #   { id: folder['id'],
+  #     name: folder['attributes']['name'],
+  #     folder_url: folder['attributes']['folder_url'],
+  #     files: files }
+  # end
 end

bd05cd60c41b5a690859eff8e7ebac76e93f140b
Erick Salinas <efsalinas00@gmail.com>
2016-06-30 07:02:57 +0800

    Convert public key from base64 and show into account view

--- services/get_public_key.rb
+++ services/get_public_key.rb
@@ -1,12 +1,19 @@
 require 'http'
-
+require 'base64'
 # Returns all folders belonging to an account
 class GetPublicKey
   def self.call(current_account:, auth_token:)
     response = HTTP.auth("Bearer #{auth_token}")
                    .get("#{ENV['API_HOST']}/accounts/#{current_account['id']}/pk")
 
-    response.code == 200 ? response.parse : nil
+    response.code == 200 ? extract_publicKey(response.parse) : nil
+  end
+
+  private_class_method
+
+  def self.extract_publicKey(response)
+    # Convert from base64
+    Base64.strict_decode64(response['public_key'])
   end
 
 end
